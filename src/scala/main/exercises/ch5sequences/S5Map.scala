966e7ba988c5d23115a25a9360f191be43abcfd4
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-09-22 10:04:05 +0100

    Maybe.flatMap

--- src/scala/main/exercises/ch5sequences/S5Map.scala
+++ src/scala/main/exercises/ch5sequences/S5Map.scala
@@ -43,12 +43,31 @@ object S5Map extends App {
 
   final case class Empty[A]() extends Maybe[A]
 
+  def mightFail1: Maybe[Int] = Full(1)
 
+  def mightFail2: Maybe[Int] = Full(2)
 
+  def mightFail3: Maybe[Int] = Empty()
 
+  val testMaybe: Maybe[Int] =
+    mightFail1 flatMap { x =>
+      mightFail2 flatMap { y =>
+        mightFail3 flatMap { z =>
+          Full(x + y + z)
+        }
+      }
+    }
 
+  println(testMaybe)
 
+  val testMaybe2: Maybe[Int] =
+    mightFail1 flatMap { x =>
+      mightFail2 flatMap { y =>
+        Full(x + y)
+      }
+    }
 
+  println(testMaybe2)
 
 
 }
